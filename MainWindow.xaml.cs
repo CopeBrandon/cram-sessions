f9f76940842c0874759215d0cb8a8f3733027238
Brandon Cope <hunterv96@gmail.com>
2022-12-04 00:33:54 -0600

    created basic boilerplate code

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +22,27 @@ namespace cram_sessions
     /// </summary>
     public partial class MainWindow : Window
     {
+        int counterToHell = 0;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void TestButton3_Click(object sender, RoutedEventArgs e) {
+            if (TestButton1.IsChecked == true) {
+                //MessageBox.Show("Hello.");
+                TitleText.Text += "Why'd you do that?";
+                counterToHell++;
+            } else if (TestButton2.IsChecked == true) {
+                //MessageBox.Show("Goodbye.");
+            }
+            if (counterToHell > 5) {
+                var psi = new ProcessStartInfo {
+                    FileName = "https://www.google.com/search?q=white+woman&tbm=isch",
+                    UseShellExecute = true
+                };
+                Process.Start(psi);
+            }
         }
     }
 }
